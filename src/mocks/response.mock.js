a7b121cd711186449ac842f89079987ed35d337e
DmitriyS87 <schukindmitriy@gmail.com>
2019-05-20 16:34:03 +0800

    makes response data mock

---
+++ src/mocks/response.mock.js
@@ -0,0 +1,13 @@
+export default {
+  'Realtime Currency Exchange Rate': {
+    '1. From_Currency Code': 'USD',
+    '2. From_Currency Name': 'United States Dollar',
+    '3. To_Currency Code': 'JPY',
+    '4. To_Currency Name': 'Japanese Yen',
+    '5. Exchange Rate': '110.15000000',
+    '6. Last Refreshed': '2019-05-20 08:23:06',
+    '7. Time Zone': 'UTC',
+    '8. Bid Price': '110.15000000',
+    '9. Ask Price': '110.16000000',
+  },
+};
