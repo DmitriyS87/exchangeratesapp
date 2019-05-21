70929dbdda49d193570b0779a0feaddfd8b4e08b
DmitriyS87 <schukindmitriy@gmail.com>
2019-05-22 03:50:00 +0800

    rebuild index.js to render App

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import style from './style/main.less';
 
 // eslint-disable-next-line import/extensions
 import App from './app.jsx';
+import data, { CURRENCIES } from './mocks/response.mock';
+import DataParcer from './utils/data-parser';
 
 const entry = document.querySelector('.app');
-const text = 'make it easy';
 
 ReactDOM.render(
-  <App text={text} style={style} />, entry,
+  <App data={DataParcer.parseCurrencyPairs(data.data)} currencies={CURRENCIES} />, entry,
 );
