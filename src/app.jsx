202a0993b7e22a8c52940131c099e3a27fe021fc
DmitriyS87 <schukindmitriy@gmail.com>
2019-05-22 15:57:02 +0800

    rebuilds App to use new ExchangeTable components

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-array-index-key */
 /* eslint-disable react/forbid-prop-types */
 /* eslint-disable import/extensions */
 import React from 'react';
@@ -5,6 +6,10 @@ import PropTypes from 'prop-types';
 import Title from './components/title.jsx';
 import Controls from './components/controlls.jsx';
 import ExchangeTable from './components/exchange-table.jsx';
+import ExchangeTableRow from './components/exchange-table-row.jsx';
+import ExchangeTableCell from './components/exchange-table-cell.jsx';
+
+import TABLE_CONSTANTS from './constants/table.constants';
 
 class App extends React.Component {
   constructor(props) {
@@ -19,14 +24,23 @@ class App extends React.Component {
 
   render() {
     const { data, currencies } = this.props;
-    console.log(data);
-    console.log(currencies);
 
     return (
       <React.Fragment>
         <Title />
         <Controls currensies={currencies} onClick={this.handleClickAdd} />
-        <ExchangeTable exchangePairs={[data]} />
+        <ExchangeTable>
+          <ExchangeTableRow style={TABLE_CONSTANTS.HEADER_ROW_STYLE} key={`row${TABLE_CONSTANTS.ROW_TITLE_INDEX}`}>
+            {TABLE_CONSTANTS.TITLES.map((title, column) => <ExchangeTableCell title={title} key={`row${TABLE_CONSTANTS.ROW_TITLE_INDEX}cell${column}`} />)}
+          </ExchangeTableRow>
+          {[data].map((row, idx) => (
+            <ExchangeTableRow style={TABLE_CONSTANTS.DATA_ROWS_STYLE} key={`row${TABLE_CONSTANTS.ROW_TITLE_INDEX + idx + 1}`}>
+              {TABLE_CONSTANTS.TITLES.map((title, column) => (
+                <ExchangeTableCell title={row[title]} key={`row${idx + 1}cell${column}`} />
+              ))}
+            </ExchangeTableRow>
+          ))}
+        </ExchangeTable>
       </React.Fragment>
     );
   }
--- src/components/exchange-table.jsx
+++ src/components/exchange-table.jsx
@@ -2,37 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const ExchangeTable = (props) => {
-  const { exchangePairs } = props;
+  const { children = '' } = props;
 
   return (
     <div className="exchange__table">
-      <div key="row0" className="exchange__row exchange__row-header">
-        <div key="#" className="exchange__cell">#</div>
-        <div key="from" className="exchange__cell">from</div>
-        <div key="to" className="exchange__cell">to</div>
-        <div key="rate" className="exchange__cell">rate</div>
-        <div key="bid" className="exchange__cell">bid</div>
-        <div key="ask" className="exchange__cell">ask</div>
-        <div key="date" className="exchange__cell">date</div>
-      </div>
-      {exchangePairs.map((pair, idx) => (
-        <div key={`row${  idx  }${1}`} className="exchange__row exchange__row-data">
-          <div key={`#${idx }${1}`} className="exchange__cell">{idx + 1}</div>
-          <div key={`from${idx }${1}`} className="exchange__cell">{pair.from}</div>
-          <div key={`to${idx}${1}`} className="exchange__cell">{pair.to}</div>
-          <div key={`rate${idx }${1}`} className="exchange__cell">{pair.rate}</div>
-          <div key={`bid${idx }${1}`} className="exchange__cell">{pair.bid}</div>
-          <div key={`ask${idx}${1}`} className="exchange__cell">{pair.ask}</div>
-          <div key={`date${idx }${1}`} className="exchange__cell">{pair.date}</div>
-        </div>
-      ))
-      }
+      {children}
     </div>
   );
 };
 
 ExchangeTable.propTypes = {
-  exchangePairs: PropTypes.arrayOf(PropTypes.object).isRequired,
+  children: PropTypes.node,
 };
 
 export default ExchangeTable;
