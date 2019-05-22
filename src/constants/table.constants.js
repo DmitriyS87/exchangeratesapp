849f622d78b7b49aa5eddb5dbeea17d53c6d6dff
DmitriyS87 <schukindmitriy@gmail.com>
2019-05-22 15:58:00 +0800

    makes table.constants

---
+++ src/constants/table.constants.js
@@ -0,0 +1,8 @@
+const TABLE_CONSTANTS = {
+  TITLES: ['#', 'from', 'to', 'rate', 'bid', 'ask', 'date'],
+  ROW_TITLE_INDEX: 0,
+  HEADER_ROW_STYLE: 'exchange__row-header',
+  DATA_ROWS_STYLE: 'exchange__row-data',
+};
+
+export default TABLE_CONSTANTS;
