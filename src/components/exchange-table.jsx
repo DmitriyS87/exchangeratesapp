2f01440af750d6cced317358af6f5a3fca748302
DmitriyS87 <schukindmitriy@gmail.com>
2019-05-22 12:46:23 +0800

    fixes bugs keys for dives

--- src/components/exchange-table.jsx
+++ src/components/exchange-table.jsx
@@ -3,26 +3,27 @@ import PropTypes from 'prop-types';
 
 const ExchangeTable = (props) => {
   const { exchangePairs } = props;
+
   return (
-    <div ClassName="exchange__table">
-      <div ClassName="exchange__row exchange__row-header">
-        <div ClassName="exchange__cell">#</div>
-        <div ClassName="exchange__cell">from</div>
-        <div ClassName="exchange__cell">to</div>
-        <div ClassName="exchange__cell">rate</div>
-        <div ClassName="exchange__cell">bid</div>
-        <div ClassName="exchange__cell">ask</div>
-        <div ClassName="exchange__cell">date</div>
+    <div className="exchange__table">
+      <div key="row0" className="exchange__row exchange__row-header">
+        <div key="#" className="exchange__cell">#</div>
+        <div key="from" className="exchange__cell">from</div>
+        <div key="to" className="exchange__cell">to</div>
+        <div key="rate" className="exchange__cell">rate</div>
+        <div key="bid" className="exchange__cell">bid</div>
+        <div key="ask" className="exchange__cell">ask</div>
+        <div key="date" className="exchange__cell">date</div>
       </div>
       {exchangePairs.map((pair, idx) => (
-        <div ClassName="exchange__row exchange__row-data">
-          <div ClassName="exchange__cell">{idx + 1}</div>
-          <div ClassName="exchange__cell">{pair.from}</div>
-          <div ClassName="exchange__cell">{pair.to}</div>
-          <div ClassName="exchange__cell">{pair.rate}</div>
-          <div ClassName="exchange__cell">{pair.bid}</div>
-          <div ClassName="exchange__cell">{pair.ask}</div>
-          <div ClassName="exchange__cell">{pair.date}</div>
+        <div key={`row${  idx  }${1}`} className="exchange__row exchange__row-data">
+          <div key={`#${idx }${1}`} className="exchange__cell">{idx + 1}</div>
+          <div key={`from${idx }${1}`} className="exchange__cell">{pair.from}</div>
+          <div key={`to${idx}${1}`} className="exchange__cell">{pair.to}</div>
+          <div key={`rate${idx }${1}`} className="exchange__cell">{pair.rate}</div>
+          <div key={`bid${idx }${1}`} className="exchange__cell">{pair.bid}</div>
+          <div key={`ask${idx}${1}`} className="exchange__cell">{pair.ask}</div>
+          <div key={`date${idx }${1}`} className="exchange__cell">{pair.date}</div>
         </div>
       ))
       }
