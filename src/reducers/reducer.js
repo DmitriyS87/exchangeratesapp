bf7081c6116b4131e5fb88965f5a0116e07a4297
DmitriyS87 <schukindmitriy@gmail.com>
2019-05-23 03:24:08 +0800

    inject LocalStore to Reducer

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -2,19 +2,27 @@ import ACTION_CONSTANT from '../constants/action.constant';
 import MESSAGE_CONSTANT from '../constants/message.constant';
 import toFixedExchangeData from '../utils/action.data';
 
+import LocalStore from '../servises/local-store';
+import SERVISES_CONSTANT from '../constants/servises.constant';
+
+const localStore = new LocalStore(SERVISES_CONSTANT.STORAGE_KEY, window.localStorage);
+const objectToArray = object => Object.keys(object).map(id => object[id]);
+
 const initialState = {
-  data: [],
+  data: objectToArray(localStore.getAll()),
   request: false,
   error: false,
   message: '',
 };
 
+
 const currencyPairsReducer = (state = initialState, action) => {
   switch (action.type) {
     case ACTION_CONSTANT.DELETE: {
       const newState = { ...state };
-      const oldData = [...newState.data];
-      newState.data = [...oldData.slice().splice(0, action.index), ...oldData.slice().splice(action.index + 1, oldData.length)];
+      const data = objectToArray(localStore.getAll());
+      localStore.rebaseItems(action.index, data.length - 1);
+      newState.data = objectToArray(localStore.getAll());
       return newState;
     }
 
@@ -28,7 +36,11 @@ const currencyPairsReducer = (state = initialState, action) => {
       const newState = {
         ...state, request: false, error: false, message: MESSAGE_CONSTANT.MSG_SUCCESS,
       };
-      newState.data = newState.data.concat({ ...toFixedExchangeData(action.data), '#': newState.data.length });
+      console.log(newState.data);
+      console.log(newState.data.length);
+
+      localStore.pushItem(newState.data.length, { ...toFixedExchangeData(action.data) });
+      newState.data = objectToArray(localStore.getAll());
       return newState;
     }
 
