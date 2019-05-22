7a635256ad8c91c34085b4f8f293db92267bd5d4
DmitriyS87 <schukindmitriy@gmail.com>
2019-05-22 12:50:37 +0800

    adds RESPONSE rom mocks to App props

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 
 // eslint-disable-next-line import/extensions
 import App from './app.jsx';
-import data, { CURRENCIES } from './mocks/response.mock';
+import data, { CURRENCIES, RESPONSE } from './mocks/response.mock';
 import DataParcer from './utils/data-parser';
 
 const entry = document.querySelector('.app');
 
 ReactDOM.render(
-  <App data={DataParcer.parseCurrencyPairs(data.data)} currencies={CURRENCIES} />, entry,
+  <App data={DataParcer.parseCurrencyPair(RESPONSE)} currencies={CURRENCIES} />, entry,
 );
