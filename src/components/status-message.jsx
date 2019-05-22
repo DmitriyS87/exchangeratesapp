5b51f156fa398ade84a99470e4011c4938a00ad2
DmitriyS87 <schukindmitriy@gmail.com>
2019-05-22 21:03:24 +0800

    makes StatusMessage component with constants

---
+++ src/components/status-message.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import MESSAGE_CONSTANT from '../constants/message.constant';
+
+const StatusMessage = (props) => {
+  const { message, error } = props;
+  const messageStyle = ((error === '') ? '' : MESSAGE_CONSTANT.STYLE_ERROR);
+  return (
+    <span className={`controls__item controls__text ${messageStyle}`}>
+      {message}
+    </span>
+  );
+};
+
+StatusMessage.propTypes = {
+  message: PropTypes.string.isRequired,
+  error: PropTypes.string.isRequired,
+};
+
+export default StatusMessage;
---
+++ src/constants/message.constant.js
@@ -0,0 +1,8 @@
+const MESSAGE_CONSTANT = {
+  STYLE_ERROR: '',
+  MSG_REQUEST: 'Loading...',
+  MSG_SUCCESS: '',
+  MSG_ERROR: 'ERROR: ',
+};
+
+export default MESSAGE_CONSTANT;
